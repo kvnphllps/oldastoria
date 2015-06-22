6862dd2da6494572b0dce79638331b51f27830ba
Kevin Phillips <kevin.g.phillips@gmail.com>
2015-06-22 07:48:24 -0700

    updated header, trying to closing in on a look: colors, nav organization, etc.

--- index.php
+++ index.php
@@ -3,6 +3,24 @@
 <head>
     <style type="text/css">
         html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
+        nav {
+            /*font-family: "Georgia", Times, serif;*/
+            font-family: futura;
+            font-size: 2em;
+            background-color: #006377;
+            color: lightgray;
+        }
+        nav ul{
+            list-style: none;
+            margin: 0px 20px; /*Top/Bottom Left/Right*/
+            padding: 5px ;
+        }
+
+        nav li{
+            display: inline-block;
+        }
+
+
     </style>
     <script type="text/javascript"
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCvwzyVrSGH229FQOFZC1NgSX3JRs1Wkng">
@@ -18,8 +36,25 @@
         }
         google.maps.event.addDomListener(window, 'load', initialize);
     </script>
+    <title>Old Astoria</title>
 </head>
+
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    Old Astoria
+                </li>
+                <li>
+                    <div style="font-size: small" style="font-family: Arial">a mapping of historical <br> photos from the CCHS</div>
+                </li>
+
+
+                <li>About</li>
+            </ul>
+        </nav>
+    </header>
 <div id="map-canvas"></div>
 </body>
 </html>
