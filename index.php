a1317002261c9c46a6de801243c3bb8e32379a82
Kevin Phillips <kevin.g.phillips@gmail.com>
2015-06-28 20:28:59 -0700

    no really, decided final default position and zoom of map

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php include('inc/header.php'); ?>
 
     <script type="text/javascript">
-        var defPos = new google.maps.LatLng(46.185, -123.8311);
+        var defPos = new google.maps.LatLng(46.183, -123.82251);
         function initializeMap() {
+
+            // Set up terrain map with a zoom of 15
             var mapOptions = {
                 center: defPos,
-                zoom: 15,
+                zoom: 14,
                 mapTypeId: google.maps.MapTypeId.TERRAIN,
                 disableDefaultUI: true,
-                zoomControl: false,
+                zoomControl: true,
                 panControl: true
             };
             var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+            // turn off points of interest
             var noPoi = [
                 {
                     featureType: "poi",
@@ -29,13 +32,8 @@
             map.setOptions({styles: noPoi});
 
             // Display our custom circle on the map
-//            var image = {
-//                url: img/dot.png,
-//                scaledSize : new google.maps.Size(5,5)
-//        }
-
             var mydot = { url: 'img/dot.png', scaledSize: new google.maps.Size(8, 8) };
-
+            // Lat/Lng of circle
             var currPos = new google.maps.LatLng(46.18955929, -123.83172154);
 
             var dotMarker = new google.maps.Marker({
@@ -47,6 +45,27 @@
                 map: map,
                 icon: mydot
             });
+
+            // Add marker window (where pictures will live)
+            var contentString =
+                '<div>'+
+                '<h1 style="float: right">Here is a picture!</h1>'+
+                '<img  style="float: right" src="img/downtown_astoria.png">'+
+                '</div>';
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString,
+                maxWidth: 1000
+//                maxHeight: 1000
+            });
+
+
+            google.maps.event.addListener(dotMarker, 'click', function() {
+//                infowindow.open(map,dotMarker);
+                infowindow.open(map);
+            });
+
+
         }
 
         google.maps.event.addDomListener(window, 'load', initializeMap);
