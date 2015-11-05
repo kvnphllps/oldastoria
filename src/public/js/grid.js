5e1ab6383e0f88528aaa2bc3280a2f6e6b730c1d
Kevin Phillips <kevin.g.phillips@gmail.com>
2015-11-05 11:53:08 -0800

    minor adjustments

--- src/public/js/grid.js
+++ src/public/js/grid.js
@@ -344,8 +344,9 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
+			this.$details = $( '<p></p>' );
 			this.$href = $( '<a href="#">Visit website</a>' );
-			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
+			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$details, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$fullimage = $( '<div class="og-fullimg"></div>' ).append( this.$loading );
 			this.$closePreview = $( '<span class="og-close"></span>' );
@@ -382,11 +383,13 @@ var Grid = (function() {
 					href : $itemEl.attr( 'href' ),
 					largesrc : $itemEl.data( 'largesrc' ),
 					title : $itemEl.data( 'title' ),
-					description : $itemEl.data( 'description' )
+					description : $itemEl.data( 'description' ),
+					details : $itemEl.data( 'details' )
 				};
 
 			this.$title.html( eldata.title );
 			this.$description.html( eldata.description );
+			this.$details.html( eldata.details );
 			this.$href.attr( 'href', eldata.href );
 
 			var self = this;
